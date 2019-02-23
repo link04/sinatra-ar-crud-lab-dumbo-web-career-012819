b394d2afcd3bffc32c864d13b2814c0429d6b385
Maximo Bautista <max.bautista97@gmail.com>
2019-02-23 20:02:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 require_relative '../../config/environment'
 require 'pry'
 class ApplicationController < Sinatra::Base
+use Rack::MethodOverride
 
   configure do
     set :public_folder, 'public'
@@ -9,6 +10,7 @@ class ApplicationController < Sinatra::Base
   end
 
   def article_id
+<<<<<<< HEAD
      Article.find(params[:id])
   end
 
@@ -48,4 +50,52 @@ class ApplicationController < Sinatra::Base
     redirect "/articles"
   end
 
+=======
+     @article = Article.find(params[:id])
+  end
+  
+  get '/index' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do 
+    
+    binding.pry
+    @article = Article.create(params)
+    redirect "/articles/#{@articles.id}"
+  end
+  
+  get '/articles/:id' do 
+    @article = article_id
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do 
+    article_id
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    article_id
+    @article.update(params[:id]).save
+    redirect "/articles/#{@article.id}"
+  end
+  
+  patch '/articles/:id' do
+    article_id
+    @article.update(params[:id]).save
+    redirect "/articles/#{@article.id}"
+  end
+  
+  delete '/articles/:id' do 
+    article_id.delete
+    redirect "/index"
+  end
+  
+>>>>>>> 0f06e356f0d918e26d5c4b2f94964345c09e8a82
 end
